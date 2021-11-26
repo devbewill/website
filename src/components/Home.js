9fa98f70e7e3002612782d7d8b8e14c7e5d7ac1d
devbewill <sedprc@gmail.com>
2021-11-26 12:43:49 +0100

    first

---
+++ src/components/Home.js
@@ -0,0 +1,36 @@
+import React from "react";
+import styled from "styled-components";
+import Section from "./Section";
+
+function Home() {
+  return (
+    <Container>
+      Home
+      <Section
+        title="Top"
+        description="lorem ipsum top"
+        leftBtnText="left btn"
+        rightBtnText="right btn"
+        backgroundImage="bg1.png"
+      ></Section>
+      <Section
+        title="Middle"
+        description="lorem ipsum middle"
+        leftBtnText="left btn"
+        backgroundImage="bg2.png"
+      ></Section>
+      <Section
+        title="Bottom"
+        description="lorem ipsum bottom"
+        leftBtnText="left btn"
+        backgroundImage="bg3.png"
+      ></Section>
+    </Container>
+  );
+}
+
+export default Home;
+
+const Container = styled.div`
+  height: 100vh;
+`;
